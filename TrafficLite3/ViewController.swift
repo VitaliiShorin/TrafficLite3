f0973441bb598bd361c304c47c7889940e4429aa
VitaliiShorin <9971425@mail.ru>
2024-11-24 13:26:20 +0300

    Initial Commit

---
+++ TrafficLite3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLite3
+//
+//  Created by Виталий Шорин on 24.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
